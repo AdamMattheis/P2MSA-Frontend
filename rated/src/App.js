9053705875865933b13d9501c922d20abbc23508
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-12-03 14:45:17 -0500

    updated home page routes

--- rated/src/App.js
+++ rated/src/App.js
@@ -1,50 +1,23 @@
 import './App.css';
-import { BrowserRouter as Router, Link, Routes, Route } from 'react-router-dom'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './Home'
 import Movies from './components/movies'
 import TvShows from './components/tvshows'
 import VideoGames from './components/videogames'
 
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Link to='/'>
-          <h1 className='title'>
-            RATED
-          </h1>
-        </Link>
-       
-      </div>
-
-      <div>
-        <ul className='background-buttons'>
-          <li>
-            <Link to='/movies'>
-              <button className='App-buttons'>Movies</button>
-            </Link>
-          </li>
-          <li>
-            <Link to='/tvshows'>
-              <button className='App-buttons'>TV Shows</button>
-            </Link>
-          </li>
-          <li>
-            <Link to='/videogames'>
-              <button className='App-buttons'>Video Games</button>
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      <div>
+      <BrowserRouter>
         <Routes>
-        <Route path='/'/>
-          <Route path='/movies' element={<Movies/>} />
-          <Route path='/tvshows' element={<TvShows/>} />
-          <Route path='/videogames' element={<VideoGames/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/movies" element={<Movies />} />
+          <Route exact path="/tvshows" element={<TvShows />} />
+          <Route exact path="/videogames" element={<VideoGames />} />
         </Routes>
-      </div>
-    </Router>    
+      </BrowserRouter>
   );
 }
 
--- rated/src/components/movies.js
+++ rated/src/components/movies.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Movies = () => {
+
+    function GetData() {
+        const [data, setData] = useState({})
+
+        useEffect(()=>{
+            //only need to fetch '/movies' because of proxy
+            fetch("/movies")
+            .then(res => res.json())
+            .then(data => setData(data))
+        }, []) 
+
+        return(
+            <div>
+                <div>{data.name}</div>
+                <div>{data.age}</div>  
+            </div>
+        ) 
+    }
+    
+    
+
   return (
       <main>
+
           <div className='moviesPage'>
               <p>list of movies</p>
+              <GetData />
           </div>
 
       </main>
