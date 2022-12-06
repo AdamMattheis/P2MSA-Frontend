37f071f3e13052528010d80d731dcbcf44de2b93
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-12-05 19:41:45 -0500

    added moviesFormatted mapping function

--- rated/src/components/movies.js
+++ rated/src/components/movies.js
@@ -12,20 +12,52 @@ const Movies = () => {
 
     useEffect(()=> {
         const fetchData = async () => {
-            const response = await fetch(`http://localhost:3000/movies`)
+            const response = await fetch(`http://localhost:5000/movies`)
             const resData = await response.json()
             setMovies(resData)
         }
         fetchData()
     }, [])
 
+    let moviesFormatted = movies.map((movie) => {
+		return (
+			<div className="col-sm-6" key={movie.movieId}>
+				<h2>
+					<button onClick={() => navigate(`/movies/${movie.movieId}`)} > 
+						{movie.name}
+                    </button> 
+				</h2>
+				<p className="text-center">
+					Rating {movie.rating}
+				</p>
+				<img style={{ maxWidth: 200 }} src={movie.pic} alt={movie.name} />
+				<p className="text-center">
+					Genre {movie.genre}
+				</p>
+                <p className="text-center">
+					Length {movie.length}
+				</p>
+			</div>
+		)
+	})
+
 
   return (
-      <main>
-          <div className='moviesPage'>
-              <p>list of movies</p>
-          </div>
-      </main>
+    <main>
+        <div className='title'>
+            <a href='/'>
+                <button className='title'>
+                    RATED
+                </button>
+            </a>
+        </div>
+        <div className='moviesPage'>
+            <p>list of movies</p>
+        </div>
+        <div>
+            {moviesFormatted}
+        </div>
+    </main>
   )
 }
 
