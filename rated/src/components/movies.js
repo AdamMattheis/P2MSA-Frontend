c99b70055ae1e4027d0d5fb54092296556c38ffe
Connor <connor5741213@gmail.com>
2022-12-08 19:27:39 -0500

    preparing for merge

--- rated/src/components/movies.js
+++ rated/src/components/movies.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import { useEffect, useState } from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 
 //import { useNavigate, useParams } from "react-router"
 
 const Movies = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-    
-    const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
 
-    useEffect(()=> {
-        const fetchData = async () => {
-            const response = await fetch(`http://localhost:3000/movies`)
-            const resData = await response.json()
-            setMovies(resData)
-        }
-        fetchData()
-    }, [])
-
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`http://localhost:3000/movies`);
+      const resData = await response.json();
+      setMovies(resData);
+    };
+    fetchData();
+  }, []);
 
   return (
-      <main>
-          <div className='moviesPage'>
-              <p>list of movies</p>
-          </div>
-      </main>
-  )
-}
+    <main>
+      <div className="moviesPage">
+        <p>list of movies</p>
+      </div>
+    </main>
+  );
+};
 
-export default Movies
+export default Movies;
